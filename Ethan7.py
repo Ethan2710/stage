290e02570d8b2118f91b320d55d970ada4ef05d4
ethan2710 <ethan.bensimon@yahoo.com>
2025-02-10 16:12:32 +0100

    aire et périmètre triangle

---
+++ Ethan7.py
@@ -0,0 +1,9 @@
+from math import *
+d = input('choisissez un nombre entier pour une longueur d un demi-périmètre ')
+a = input('choisissez un nombre entier pour une longueur d un coté du triangle ')
+b = input('choisissez un nombre entier pour une longueur d un autre coté du triangle ')
+c = input('choisissez un nombre entier pour une longueur d un autre coté du triangle ')
+périmètre = a + b + c
+aire = (sqrt(int(d)*(int(d)-int(a))*(int(d)-int(b))*(int(d)-int(c))))
+print(périmètre)
+print(aire)
