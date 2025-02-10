63a0dd6f6280c9c3c896ad880d1775df149a9223
ethan2710 <ethan.bensimon@yahoo.com>
2025-02-10 11:20:51 +0100

    calculatrice

---
+++ Ethan3.py
@@ -0,0 +1,11 @@
+chiffre1 = input(" écris_moi un premier nombre ")
+chiffre2 = input(" écris_moi un deuxième nombre")
+calcul = input ("écris moi un signe de calcul")
+if calcul == '+':
+    print( int(chiffre1)+ int(chiffre2))
+if calcul == '-':
+    print( int(chiffre1)- int(chiffre2))
+if calcul == '*':
+    print( int(chiffre1)* int(chiffre2))
+if calcul == '/':
+    print( int(chiffre1)/ int(chiffre2))
