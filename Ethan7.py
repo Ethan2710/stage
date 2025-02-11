e29101e13dce103a0110f9ae2c2d4c27f65aa65a
ethan2710 <ethan.bensimon@yahoo.com>
2025-02-11 11:23:36 +0100

    aire et périmètre d'un triangle

--- Ethan7.py
+++ Ethan7.py
@@ -4,6 +4,12 @@ a = input('choisissez un nombre entier pour une longueur d un coté du triangle 
 b = input('choisissez un nombre entier pour une longueur d un autre coté du triangle ')
 c = input('choisissez un nombre entier pour une longueur d un autre coté du triangle ')
 périmètre = a + b + c
-aire = (sqrt(int(d)*(int(d)-int(a))*(int(d)-int(b))*(int(d)-int(c))))
+expression = int(d)*(int(d)-int(a))*(int(d)-int(b))*(int(d)-int(c))
+
+if expression < 0:
+    print("Erreur : l'expression est négative")
+    aire = None
+else:
+    aire = (sqrt(expression))
 print(périmètre)
 print(aire)
