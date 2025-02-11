c5b690ab0e059798a679307d2be7629e688517af
ethan2710 <ethan.bensimon@yahoo.com>
2025-02-11 19:27:32 +0100

    Volume d'un cône

--- Ethan7B.py
+++ Ethan7B.py
@@ -1,5 +1,5 @@
 from math import *
-print('Nous allons vous claculer l aire d un cone en mètres carres')
+print('Nous allons vous claculer le volumed un cone en mètres cube')
 rayon = input('choisissez un nombre entier pour une longueur d un rayon')
 hauteur = input('choisissez un nombre entier pour une longueur d une hauteur')
 try:
@@ -12,4 +12,4 @@ volume_cône = volume_cilyndre/3
 if volume_cône < 0:
     print("Erreur : l'expression est négative")
     aire = None
-print('le résultat est :', volume_cône, 'm²')
+print('le résultat est :', volume_cône, 'm3')
