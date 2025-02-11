46bb8a14cc366ce604aef05e20ecf9f592f1ee69
ethan2710 <ethan.bensimon@yahoo.com>
2025-02-11 11:44:40 +0100

    aire cône

---
+++ Ethan7B.py
@@ -0,0 +1,15 @@
+from math import *
+print('Nous allons vous claculer l aire d un cone en mètres carres')
+rayon = input('choisissez un nombre entier pour une longueur d un rayon')
+hauteur = input('choisissez un nombre entier pour une longueur d une hauteur')
+try:
+    rayon = int(rayon)
+    hauteur = int(hauteur)
+    volume_cilyndre = 3.14 * rayon**2 * hauteur
+except ValueError:
+    print("Erreur : les valeurs de rayon et hauteur ne sont pas numériques")
+volume_cône = volume_cilyndre/3
+if volume_cône < 0:
+    print("Erreur : l'expression est négative")
+    aire = None
+print('le résultat est :', volume_cône, 'm²')
